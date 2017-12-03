60b92a358671f37d1f70876e0ddb6d7b63cbec38
yuhei matsuda <yuhei.matsuda.2017b@mlab.info>
2017-12-03 12:26:13 +0900

    yeah

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -2,6 +2,8 @@ class TalksController < ApplicationController
   before_action :authenticate_user, {only: [:index , :show]}
 
   def index
+    @users = User.all
+    @user_current = session
   end
 
   def show
