440b1bb709fc3aa4b8ffd7facaa29e8524d3ef0e
yuhei matsuda <yuhei.matsuda.2017b@mlab.info>
2017-12-03 11:35:55 +0900

    マージしきれてなかった

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,5 +1,11 @@
 class TalksController < ApplicationController
+  before_action :authenticate_user, {only: [:index , :show]}
+
   def index
   end
-  
+
+  def show
+    @talk = Talk.find_by(id: params[:id])
+  end
+
 end
